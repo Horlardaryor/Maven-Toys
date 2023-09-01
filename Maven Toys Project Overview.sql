d97a47b12ed94e20aadfee7f93cbb192908f2c5e
Oladayo Ogunyomi <133796054+Horlardaryor@users.noreply.github.com>
2023-09-01 14:32:19 -0600

    Update Maven Toys Project Overview.sql

--- Maven Toys Project Overview.sql
+++ Maven Toys Project Overview.sql
@@ -1,122 +1,30 @@
-•	Question 1:
-/* Which product categories drive the biggest profits? Is this the same across store locations? */
+/* Project Overview: Sales & Inventory Analysis for Maven Toys
 
-WITH category_profit AS (
-    SELECT
-        products.product_category,
-        stores.store_location,
-        SUM(products.product_price - products.product_cost) AS category_profit
-    FROM
-        products
-    JOIN inventory ON products.product_id = inventory.product_id
-    JOIN stores ON inventory.store_id = stores.store_id
-    GROUP BY
-        products.product_category,
-        stores.store_location
-)
+In my capacity as a data analyst, my capstone project was centered around the examination of sales and inventory data for Maven Toys, a fictitious chain of toy stores located in Mexico. The primary aim of this project was to glean valuable insights pertaining to product profitability, inventory management, and the repercussions of product stockouts on sales. With a professional tone, the following provides a comprehensive overview of the project:
 
-SELECT
-    cp.product_category,
-    cp.store_location,
-    cp.category_profit
-FROM
-    category_profit AS cp
-ORDER BY category_profit DESC
-  
- /* Result: Toys category drives the biggest profits amongst the product categories at Maven Toys. Also, Toys drives the biggest profits across store locations.
+Data Scope:
+This project entailed the meticulous analysis of extensive sales and inventory data, encompassing intricate details about the products, store locations, daily sales transactions, and the prevailing inventory levels at each of the Maven Toys outlets. This dataset proved to be an invaluable resource, offering a wealth of information for comprehensive analysis and informed decision-making.
 
+Objective:
+The core objective of this project revolved around addressing pivotal business queries and furnishing actionable insights aimed at bolstering profitability, streamlining inventory levels, and mitigating sales losses arising from product unavailability.
 
-/* Q2: How much money is tied up in inventory at the toy stores? How long will it last? */
-	
-SELECT
-		SUM (stock_on_hand * product_cost) AS money_tied_up
-    FROM
-        inventory
-    JOIN products ON products.product_id = inventory.product_id 
-	WHERE
-    product_category = 'Toys'
-	
-/* Part A Result: $99,861.47 is tied up in inventory at the Toys stores. */
+Methodology:
+To effectively fulfill the project objectives, I employed SQL queries and data analysis techniques to extract and decipher meaningful patterns from the sales and inventory dataset. The analysis focused on providing answers to the following critical inquiries:
 
-WITH average_daily_sales AS (
-    SELECT
-        product_id,
-        AVG(units) AS average_daily_sales
-    FROM
-        sales
-    GROUP BY
-        product_id
-)
+Product Category Profitability:
+By utilizing SQL queries and conducting pertinent calculations, I identified the product categories that contributed the most to Maven Toys' profits. Additionally, I delved into whether these lucrative product categories were consistent across all store locations or if certain outlets exhibited distinct profit-driving categories.
 
-SELECT
-    AVG(i.stock_on_hand / s.average_daily_sales) AS estimated_days_last
-FROM
-    inventory AS i
-JOIN
-    products AS p ON p.product_id = i.product_id
-JOIN
-    average_daily_sales AS s ON s.product_id = p.product_id
-WHERE
-    p.product_category = 'Toys';
+Inventory Assessment:
+In order to gauge the capital tied up in inventory, I computed the overall value of inventory across all Maven Toys stores. Taking into account the average daily sales and inventory levels, I ascertained the duration for which the current inventory could sustain operations. This analysis yielded valuable insights into inventory turnover and efficiency.
 
-/* Part B Result: It would last for about 14 days. */
+Impact of Stockouts on Sales:
+Employing SQL queries and sales transaction data, I conducted an exhaustive examination to determine if sales were being adversely affected by product unavailability at specific Maven Toys locations. By identifying instances of product stockouts and closely scrutinizing the corresponding sales data, I quantified the financial ramifications and recommended strategies to curtail sales losses.
+
+Conclusion:
+The sales and inventory analysis conducted for Maven Toys yielded invaluable insights into product profitability, inventory management, and the consequences of stockouts on sales. The findings of this project empowered Maven Toys to make well-informed decisions aimed at enhancing profitability, optimizing inventory levels, and ensuring customer satisfaction by minimizing lost sales arising from product unavailability.
+
+In summary, this project underscored the pivotal role of data analysis in bolstering strategic decision-making for Maven Toys. It provided the company with the means to implement data-driven enhancements to its sales and inventory management processes, thus contributing to the organization's overall success.*/
 
 
-
-/* Q3: Are sales being lost with out-of-stock products at certain locations? */
- 
-WITH sales_inventory AS (
-    SELECT
-        s.sale_id,
-        i.stock_on_hand,
-        s.date,
-        st.store_location,
-        p.product_name
-    FROM
-        sales AS s
-    JOIN
-        stores AS st ON s.store_id = st.store_id
-    JOIN
-        products AS p ON s.product_id = p.product_id
-    LEFT JOIN
-        inventory AS i ON s.store_id = i.store_id AND s.product_id = i.product_id
-    WHERE
-        i.stock_on_hand IS NULL
-        OR i.stock_on_hand = 0
-)
-
-SELECT *
-FROM sales_inventory;
-
-
-/* Yes. The data shows that sales are being lost in certain location as a result of out to stock products. Running a further query as shown below, it
-it was discovered that 31866 sales was lost across 4 different locations */
-
-
-WITH lost_sales_by_location AS (
-SELECT
-    DISTINCT (st.store_location) AS store_location,
-	COUNT (s.sale_id) AS lost_sales
-FROM
-    sales AS s
-JOIN
-    stores AS st ON s.store_id = st.store_id
-JOIN
-    products AS p ON s.product_id = p.product_id
-LEFT JOIN
-    inventory AS i ON s.store_id = i.store_id AND s.product_id = i.product_id
-WHERE
-    i.stock_on_hand IS NULL
-    OR i.stock_on_hand = 0
-GROUP BY st.store_location
-)
-
-SELECT 
-	*
-FROM
-	lost_sales_by_location
-
-/* While further analysis showed that there was sales lost accross the four store locations, Downstown store lost the most sales (19017) 
-and Airport Store had the least lost sales (1569). The query below  provides the business insights*/
 
 
